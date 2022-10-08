138d3f4ac4ba314f0aac2dee26e0b63354e62f41
Luis <luis200107@icloud.com>
2022-10-08 12:35:26 -0500

    Añadiendo listas circulares y adts necesarios para tarea 6

---
+++ EstructuraDeDatos/src/aragon/unam/tareas/TareaSeis/PruebasTareaSeis.java
@@ -0,0 +1,11 @@
+package aragon.unam.tareas.TareaSeis;
+
+import java.util.Iterator;
+
+import aragon.unam.estructuras.ArrayADT;
+
+public class PruebasTareaSeis {
+	public static void main(String[] args) {
+		Laberinto l = new Laberinto();
+	}
+}
