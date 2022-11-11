eddc395e4ecb1f9b010f711c0eb2c4e9bc6051cc
Luis <luis200107@icloud.com>
2022-11-10 23:50:24 -0600

    Iniciando la creación de la tabla hash

---
+++ EstructuraDeDatos/src/aragon/unam/estructuras/HashTableADT.java
@@ -0,0 +1,16 @@
+package aragon.unam.estructuras;
+
+public class HashTableADT <T>{
+	private ArrayADT<T> tabla;
+	
+	public HashTableADT(int tamanio) throws Throwable {
+		if(tamanio >= 7 && tamanio % tamanio == 0 && tamanio % 1 == 0) {
+		this.tabla = new ArrayADT<>(tamanio);
+		}else {
+			throw new Exception("El tamanio debe ser mayor "
+					+ "a 7 y un numero primo");
+		}
+	}
+	
+	
+}
