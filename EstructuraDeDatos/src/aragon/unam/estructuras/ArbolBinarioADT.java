5e3f962fbe7af831c14075130b6a7d83552a30a0
Luis <luis200107@icloud.com>
2023-01-02 21:16:26 -0600

    Guardado Dos arbol binario de busqueda

--- EstructuraDeDatos/src/aragon/unam/estructuras/ArbolBinarioADT.java
+++ EstructuraDeDatos/src/aragon/unam/estructuras/ArbolBinarioADT.java
@@ -1,5 +1,44 @@
 package aragon.unam.estructuras;
 
 public class ArbolBinarioADT {
+	private NodoArbol<Integer> head;
+	private int tamnio = 0;
 
+	public ArbolBinarioADT() {
+
+	}
+
+	public void insert(int valor) {
+		int num = 0;
+		NodoArbol<Integer> aux = this.head;
+		while (true) {
+			if (aux == null) {
+				aux.setDato(valor);
+				break;
+			} else {
+				num = aux.getDato();
+				if (valor > num) {
+					aux = aux.getNodoDerecho();
+				} else {
+					if (valor < num) {
+						aux = aux.getNodoIzquierdo();
+					} else {
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	public void remover(int valor) {
+		
+	}
+	
+	public void search(int valor) {
+		
+	}
+	
+	public String transversal (int formato) {
+		return ""; 
+	}
 }
